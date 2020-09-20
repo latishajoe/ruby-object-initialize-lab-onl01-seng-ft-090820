df42817b98f7fc17d9ffaee30592204421fbf647
Latisha Joe <latisha_joe@yahoo.com>
2020-09-20 10:02:03 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog  
+  def initialize(breed)
+    @breed = breed   
+  end 
+
+  def breed=(breed)
+    @breed = breed 
+  end 
+
+ def breed  
+   @breed  
+ end 
+
+def breed=(mutt)
+  @breed = "Mutt"
+end 
+
+def breed 
+  "Mutt" 
+end 
+  
+
+end 
+    
